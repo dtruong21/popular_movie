310c16a473fbf8f0617aa1ef2761c09020ce43c8
cminhtruong <cminh.truong@gmail.com>
2018-03-12 00:25:05 +0100

    Init the project

---
+++ app/src/main/java/com/cmtruong/udacity/configs/Config.java
@@ -0,0 +1,14 @@
+package com.cmtruong.udacity.configs;
+
+/**
+ * Created by davidetruong on 12/03/2018.
+ * @author davidetruong
+ * @version 1.0
+ */
+
+public class Config {
+
+    public static final String API_URL = "https://api.themoviedb.org/3/movie/";
+    public static final String POPULAR = "popular?api_key";
+    public static final String API_KEY = "";
+}
